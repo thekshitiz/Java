578531c4e31e660656fb1d8e26ac9dea019d7307
Kshitiz Rai <dikhwachha@gmail.com>
2022-03-28 17:19:12 +0545

    oddOrEven

---
+++ OddOrEven.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class OddOrEven {
+    public static boolean isEven(int n) {
+        return n%2==0;}
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int userNum = scanner.nextInt();
+        System.out.println("Is the number even? " + isEven(userNum));
+
+    }
+}
